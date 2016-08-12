8a84833f6613bd87f05093a784021b0292d733f5
Daniel Stahl <danny.stahl@gmail.com>
2016-08-11 21:40:53 -0700

    Created about me page

---
+++ app/controllers/about_me_controller.rb
@@ -0,0 +1,4 @@
+class AboutMeController < ApplicationController
+	def aboutme
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get 'welcome' => 'pages#welcome'
+  get 'aboutme' => 'about_me#aboutme'
   get 'projects' => 'projects#projects'
+
 end
