d4e80e85a172b68df01d3ddae1b3582849052dc5
Daniel Stahl <danny.stahl@gmail.com>
2016-08-21 12:28:18 -0700

    Setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#welcome'
+  resources :contacts, only: [:new, :create]
   get 'welcome' => 'pages#welcome'
   get 'aboutme' => 'about_me#aboutme'
   get 'projects' => 'projects#projects'
+  root 'pages#welcome'
 
 end
